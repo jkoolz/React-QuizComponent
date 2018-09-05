82bc303f99e7c3423c2b744e67e19389f97ee2b7
Jack Kooley <jkooley@gmail.com>
2018-09-05 14:15:58 -0700

    module one fixed

--- src/quiz.js
+++ src/quiz.js
@@ -4,15 +4,12 @@ let quizData = require('./quiz_data.json')
 
 class Quiz extends Component
 {
-  constructor(props)
-  {
+  constructor(props) {
     super(props)
     this.state = { quiz_position: 1 }
   }
-  render()
-  {
-    return
-    (
+  render() {
+    return (
       <div><div className="QuizQuestion">{quizData.quiz_questions[0]
         .instruction_text}</div></div>
     )
